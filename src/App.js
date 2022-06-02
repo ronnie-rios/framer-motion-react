e52abb543c7e1556dd2bbbbf74618223e2cab734
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-01 21:14:00 -0500

    installed dev dependency terser webpack, fixed route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Components from './components/Components';
+import Hooks from './components/Hooks';
+import Props from './components/Props';
+import State from './components/State';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
 
-      </header>
+      <Router>
+        <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/components' element={<Components />} />
+        <Route path='/hooks' element={<Hooks />} />
+        <Route path='/state' element={<State />} />
+        <Route path='/props' element={<Props />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
